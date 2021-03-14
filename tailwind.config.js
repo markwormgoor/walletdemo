12c633b877015da6a65a24cab7be7c46ff51dd03
Mark Wormgoor <mark@wormgoor.com>
2021-03-14 22:12:11 +0100

    Start adding tailwind for styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", 'sans']
+    },
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
