794f0bb3eece19282f5c21dd481ac6e05141f363
Mark Wormgoor <mark@wormgoor.com>
2021-03-17 16:35:16 +0100

    Create our first register screen!

--- app/routes/idone.js
+++ app/routes/idone.js
@@ -1,86 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var db = require ('../services/MongoService');
 var agentService = require('../services/AgentService');
 
 /* Get started with our walkthrough. */
-router.get('/', function(req, res, next) {
-  res.redirect  ('/wallet');
+router.get('/register-app', function(req, res, next) {
+  res.render('id1-regapp1', { title: 'Identity one: Register your application'});
 });
 
-/* Delete my session and restart */
-router.get('/restart', function(req, res, next) {
-  req.session.destroy();
-  res.redirect  ('/wallet');
-});
+router.post('/register-app', async function(req, res, next) {
+  const appname = req.body.appname;
+  const appmail = req.body.appmail;
+  if (appname.length > 2 && appmail.length > 2) {
+    // Create wallet seed
+    const Crypto = require('crypto');  
+    key = Crypto.randomBytes(24).toString('base64').slice(0,24);
 
-/* Display my current wallet */
-router.get('/wallet', function(req, res, next) {
-  if (req.session.wallet) {
-    conns = listMultitenantWalletConnections(req.session.wallet.token);
-    res.render('wallet', { title: 'Afoo Demo: Wallet', wallet: req.session.wallet});
-  } else {
-    res.render('walletmissing', { title: 'Afoo Demo: No Wallet'});
-  }
-});
-
-/* Create a new wallet */
-router.post('/wallet', async function(req, res, next) {
-  const Crypto = require('crypto');
-  key = Crypto.randomBytes(24).toString('base64').slice(0,24);
-
-  // Create wallet
-  var walletBuild = {};
-  walletBuild['image_url'] = 'https://aforceof.one/favicon.png';
-  walletBuild['key_management_mode']='managed';
-  walletBuild['label']=req.body.name;
-  walletBuild['wallet_dispatch_type']='default';
-  walletBuild['wallet_key']=key;
-  walletBuild['wallet_name']=req.body.email;
-  walletBuild['wallet_type']='indy';
-  walletBuild['wallet_webhook_urls']=['http://localhost:8022/webhooks'];
-  const walletBuildString=JSON.stringify(walletBuild);
-
-  try {
+    // Create wallet
+    var walletBuild = {};
+    walletBuild['image_url'] = '';
+    walletBuild['key_management_mode']='managed';
+    walletBuild['label']=appname;
+    walletBuild['wallet_dispatch_type']='default';
+    walletBuild['wallet_key']=key;
+    walletBuild['wallet_name']=appmail;
+    walletBuild['wallet_type']='indy';
+    walletBuild['wallet_webhook_urls']=['http://localhost:3000/webhooks'];
+    const walletBuildString=JSON.stringify(walletBuild);
     const wallet = await agentService.createMultitenantWallet(walletBuildString);
-    req.session.wallet=wallet;
-    res.redirect  ('/wallet');
-  } catch (error) {
-    next (error);
+    console.log('Created new wallet for application (' + appname + '): ' + wallet.wallet_id)
+
+    const DID = await agentService.createMultitenantWalletDID(wallet.token);
+    console.log('Created new DID for application (' + appname + '): ' + DID.result.did);
+    console.log('with verification key (' + appname + '): ' + DID.result.verkey);
+
+    // Get our global Endorser DID
+    var settingsquery = { name: "identity.one" };
+    db.get().collection("settings").find(settingsquery).toArray (function (err, results){
+      if (results[0].walletid.length < 1 || err) throw err;
+      globalDID = results[0].walletid;
+      console.log('Retrieved our global Endorser ID from DB: ' + globalDID);
+    });
+
+  } else {
+    res.render('id1-regapp1', { title: 'Identity one: Register your application'});
   }
 });
-
-/* List all wallet */
-router.get('/wallets', async function(req, res, next) {
-  try {
-    const wallets = await agentService.listMultitenantWallets();
-    res.render('walletlist', { title: 'Afoo Demo: Wallet List', wallets: wallets['results']});
-  } catch (error) {
-    next (error);
-  }
-});
-
-/* Adopt a wallet */
-router.post('/adopt', async function(req, res, next) {
-  try {
-    const wallet = await agentService.getMultitenantWallet(req.body.wallet_id);
-    req.session.wallet=wallet;
-    const token = await agentService.getMultitenantWalletToken(req.body.wallet_id);
-    wallet.token = token.token;
-    res.redirect  ('/wallet');
-  } catch (error) {
-    next (error);
-  }
-});
-
-/* Delete a wallet */
-router.post('/delete', async function(req, res, next) {
-  try {
-    const wallets = await agentService.deleteMultitenantWallet(req.body.wallet_id);
-    res.redirect  ('/wallets');
-  } catch (error) {
-    next (error);
-  }
-});
-
 
 module.exports = router;
